887b79297f3a578e64b4c4b65bfc9a8987c43045
Guo Junjun <guojunjun@gmail.com>
2015-01-05 15:40:34 +0100

    bevegelig grafikk stickman2

--- java_fx/basic/src/BevegeligController.java
+++ java_fx/basic/src/BevegeligController.java
@@ -1,5 +1,38 @@
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Shape;
+
 /**
  * Created by GuoJunjun on 05.01.15.
  */
 public class BevegeligController {
+    @FXML
+    Node stickMan, armsAndLegs1, armsAndLegs2;
+    @FXML
+    Shape eye;
+
+    boolean step = true;
+
+    @FXML
+    public void initialize() {
+        update();
+    }
+
+    @FXML
+    public void walk() {
+        step = !step;
+        stickMan.setLayoutX((stickMan.getLayoutX() + 5));
+        update();
+    }
+
+    void update() {
+        if (step) {
+            eye.setFill(Color.BROWN);
+        } else {
+            eye.setFill(Color.WHITE);
+        }
+        armsAndLegs1.setVisible(step);
+        armsAndLegs2.setVisible(!step);
+    }
 }
