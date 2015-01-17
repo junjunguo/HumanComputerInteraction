a1ae674421afec5c8bd783e52da451e4d00d2ff2
Guo Junjun <guojunjun@gmail.com>
2015-01-17 20:41:05 +0100

    work with textfield listener

--- currencyConverter/currencyConverter/Utility.java
+++ currencyConverter/currencyConverter/Utility.java
@@ -19,13 +19,19 @@ import org.json.JSONObject;
 /** Created by GuoJunjun <junjunguo.com> on 16.01.15. */
 
 public class Utility {
-    private static TextField exchange1 = new TextField();
-    private static Label     exchange2 = new Label();
-    private static Label     exchange3 = new Label();
-    private static Label     exchange4 = new Label();
+    private static       TextField exchange1 = new TextField();
+    private static       Label     exchange2 = new Label();
+    private static       Label     exchange3 = new Label();
+    private static       Label     exchange4 = new Label();
+    private static final Label     currency1 = new Label();
+    private static final Label     currency2 = new Label();
+    private static final Label     currency3 = new Label();
+    private static final Label     currency4 = new Label();
+    private static       double    rate12    = 0, rate13 = 0, rate14 = 0;
 
-    private static String curreny1, curreny2, curreny3, curreny4;
-//    private static double inputValue;
+
+    //    private static String curreny1, curreny2, curreny3, curreny4;
+    //    private static double inputValue;
 
     /**
      * @return a flow pane with all information in side
@@ -42,52 +48,111 @@ public class Utility {
     }
 
     private static GridPane GP1() {
-        final Label currency = new Label();
+
         final ImageView imageView = new ImageView();
         final ComboBox comboBox = Countries.addComboBox();
-        GridPane gp = addGridPane(true, null, comboBox, imageView, currency);
-        return gp;
-    }
+        GridPane gp = addGridPane(comboBox, imageView, currency1);
+        //        String[] table = comboBox.getSelectionModel().getSelectedItem().toString().split(" - ");
+        //        System.out.println("flags/" + table[2] + ".png");
+        imageView.setImage(new
+                                   Image(
+                "flags/" + comboBox.getSelectionModel().getSelectedItem().toString().split(" - " +
+                                                                                           "")
+                        [2] + ".png"));
 
-    private static GridPane GP2() {
-        final Label currency = new Label();
-        final ImageView imageView = new ImageView();
-        final ComboBox comboBox = Countries.addComboBox();
-
-        GridPane gp = addGridPane(false, exchange2, comboBox, imageView, currency);
-        return gp;
-    }
-
-    private static GridPane GP3() {
-        final Label currency = new Label();
-        final ImageView imageView = new ImageView();
-        final ComboBox comboBox = Countries.addComboBox();
-
-        GridPane gp = addGridPane(false, exchange3, comboBox, imageView, currency);
-        return gp;
-    }
-
-    private static GridPane GP4() {
-        final Label currency = new Label();
-        final ImageView imageView = new ImageView();
-        final ComboBox comboBox = Countries.addComboBox();
-
-        GridPane gp = addGridPane(false, exchange4, comboBox, imageView, currency);
 
         // listener currency changed
         comboBox.valueProperty().addListener(new ChangeListener() {
             @Override
             public void changed(ObservableValue observable, Object oldValue, Object newValue) {
-                setExchangeImage(exchange4, imageView, newValue.toString());
+                String[] table = newValue.toString().split(" - ");
+                System.out.println("flags/" + table[2] + ".png");
+                imageView.setImage(new Image("flags/" + table[2] + ".png"));
+                exchange2.setText(getExchangeRate(currency1.getText().substring(0, 3),
+                                                  currency2.getText().substring(0, 3),
+                                                  getInputValue(), rate12));
+                exchange3.setText(getExchangeRate(currency1.getText().substring(0, 3), currency3.getText().substring(0,
+                                                                                                                     3),
+                                                  getInputValue(), rate13));
+                exchange4.setText(getExchangeRate(currency1.getText().substring(0, 3), currency4.getText().substring(0,
+                                                                                                                     3),
+                                                  getInputValue(), rate14));
             }
         });
-        //            Label exchange = new Label();
+        exchange1.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                System.out.println(newValue);
+                if (isDouble(newValue)) {
+                    double value = Double.parseDouble(newValue);
+                    System.out.println(rate12);
+                    System.out.println(newValue);
+                    String s1 = (value * rate12) + "";
+                    System.out.println(s1);
+                    exchange2.setText(s1);
+                    exchange3.setText((value * rate13) + "");
+                    exchange4.setText((value * rate14) + "");
+                    System.out.printf((value * rate14) + "");
+                }
+            }
+        });
+
+        exchange1.setAlignment(Pos.CENTER_RIGHT);
+        gp.add(exchange1, 2, 0);
+        return gp;
+    }
+
+    private static GridPane GP2() {
+        final ImageView imageView = new ImageView();
+        final ComboBox comboBox = Countries.addComboBox();
+
+        GridPane gp = addGridPane(comboBox, imageView, currency2);
+        setExchangeImage(exchange2, imageView, comboBox.getSelectionModel().getSelectedItem().toString(), rate12);
+
+        // listener currency changed
+        comboBox.valueProperty().addListener(new ChangeListener() {
+            @Override
+            public void changed(ObservableValue observable, Object oldValue, Object newValue) {
+                setExchangeImage(exchange2, imageView, newValue.toString(), rate12);
+            }
+        });
+        exchange2.setAlignment(Pos.CENTER_RIGHT);
+        gp.add(exchange2, 2, 0);
+        return gp;
+    }
+
+    private static GridPane GP3() {
+        final ImageView imageView = new ImageView();
+        final ComboBox comboBox = Countries.addComboBox();
+
+        GridPane gp = addGridPane(comboBox, imageView, currency3);
+        setExchangeImage(exchange3, imageView, comboBox.getSelectionModel().getSelectedItem().toString(), rate13);
+        // listener currency changed
+        comboBox.valueProperty().addListener(new ChangeListener() {
+            @Override
+            public void changed(ObservableValue observable, Object oldValue, Object newValue) {
+                setExchangeImage(exchange3, imageView, newValue.toString(), rate13);
+            }
+        });
+        exchange3.setAlignment(Pos.CENTER_RIGHT);
+        gp.add(exchange3, 2, 0);
+        return gp;
+    }
+
+    private static GridPane GP4() {
+        final ImageView imageView = new ImageView();
+        final ComboBox comboBox = Countries.addComboBox();
+        GridPane gp = addGridPane(comboBox, imageView, currency4);
+        setExchangeImage(exchange4, imageView, comboBox.getSelectionModel().getSelectedItem().toString(), rate14);
+        // listener currency changed
+        comboBox.valueProperty().addListener(new ChangeListener() {
+            @Override
+            public void changed(ObservableValue observable, Object oldValue, Object newValue) {
+                setExchangeImage(exchange4, imageView, newValue.toString(), rate14);
+            }
+        });
         exchange4.setAlignment(Pos.CENTER_RIGHT);
-
-        //            restcbb(comboBox);
         gp.add(exchange4, 2, 0);
-
-
         return gp;
     }
 
@@ -96,8 +161,7 @@ public class Utility {
      *
      * @return a universal grid pane with general information
      */
-    private static GridPane addGridPane(Boolean first, final Label exchange, final ComboBox comboBox, ImageView
-            imageView, final Label currency) {
+    private static GridPane addGridPane(final ComboBox comboBox, ImageView imageView, final Label currency) {
         GridPane grid = initGridPane();
 
         // column 1, row 1 ----------------->
@@ -137,11 +201,6 @@ public class Utility {
                 }
             }
         });
-        if (first) {
-        } else {
-            //set exchange
-//            exchange.setText((getExchange(curreny1, comboBox.getValue().toString().substring(0,3), getInputValue())));
-        }
         grid.add(comboBox, 1, 0);
         grid.add(currency, 1, 0);
         return grid;
@@ -177,20 +236,19 @@ public class Utility {
     }
 
     /**
+     * *
+     *
      * @param fromCurrency
      * @param toCurrency
-     * @param value
-     * @return calculated exchange result as a string
+     * @return double rate
      */
-    private static String getExchange(String fromCurrency, String toCurrency, double value) {
+    private static double getExchangeRate(String fromCurrency, String toCurrency) {
         try {
-            JSONObject currencyrate = new JSONObject(JsonReader.getCurrency(fromCurrency, toCurrency));
-            System.out.println(currencyrate);
-
+            return new JSONObject(JsonReader.getCurrency(fromCurrency, toCurrency)).getDouble("rate");
         } catch (JSONException e) {
-            System.out.println(" getExchange: " + e);
+            System.out.println(" getExchangeRate: " + e);
         }
-        return null;
+        return 0;
     }
 
     /**
@@ -219,16 +277,18 @@ public class Utility {
     }
 
     /**
-     * set image and exchange value in to the given variable
+     * set image , exchange value and the rate in to the given variable
      *
-     * @param exchange
+     * @param exchange  Label calculated exchange value
      * @param imageView
-     * @param item
+     * @param item      The selected item from combo box
      */
     private static void setExchangeImage(Label exchange, ImageView imageView, String item) {
         String[] table = item.toString().split(" - ");
-        System.out.println("flags/" + table[2] + ".png");
         imageView.setImage(new Image("flags/" + table[2] + ".png"));
-        exchange.setText(getExchange(curreny1, curreny2, getInputValue()));
+
+        exchange.setText(
+                getExchangeRate(currency1.getText().substring(0, 3), item.substring(0, 3)) * getInputValue() + "");
+
     }
 }
